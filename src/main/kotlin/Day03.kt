e04bf43363c54e68b7355b04b3655ff692151e73
Thomas Vigh Sørensen <tvso@its.aau.dk>
2024-12-09 11:28:00 +0100

    D3P2 progress

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -23,19 +23,29 @@ private fun partOneTest() {
 private fun partOne(input: String): Int {
     val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
 
-    return regex.findAll(input).asSequence().sumOf { result ->
-        val a = result.groupValues[1].toInt()
-        val b = result.groupValues[2].toInt()
-        println("$a * $b")
-        result.groupValues[1].toInt() * result.groupValues[2].toInt()
-    }
+    return regex.findAll(input)
+        .asSequence()
+        .sumOf { result ->
+            result.groupValues[1].toInt() * result.groupValues[2].toInt()
+        }
 
 }
 
-private fun partTwoTest(): Int {
-    TODO()
+private fun partTwoTest() {
+    val input = readInput("Day03_test.txt").joinToString(" ")
+    val result = partTwo(input)
+
+    assert(result == 48) { println("Test failed! Actual was: $result") }
+    println("partTwoTest successful!")
 }
 
 private fun partTwo(input: String): Int {
-    TODO()
+    val filterRegex = """don't\(\).*do\(\)""".toRegex()
+
+    val cleaned = filterRegex.findAll(input).asSequence().map { result ->
+        val substr = result.groupValues[0]
+        input.replace(substr, "")
+    }.joinToString("")
+
+    return partOne(cleaned)
 }
